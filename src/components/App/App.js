13375e39e8301ef836f993ec7a5728945087e65a
level7dc <6236517+level7dc@users.noreply.github.com>
2020-06-13 16:21:15 +0100

    updates App syntax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,14 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-// function App() {
-class App extends React.Component {
-  render() {
-    return (
+function App() {
+  return (
     <div className="App">
     <h1>ravenous</h1>
     <SearchBar />
     <BusinessList />
   </div>
   )
-}
-}
+};
+
 export default App;
