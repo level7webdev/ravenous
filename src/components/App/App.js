e0c8b8b84a4463ae07436ab0786c198ac9776373
level7dc <6236517+level7dc@users.noreply.github.com>
2020-06-13 16:22:44 +0100

    includes app syntax updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,10 +6,10 @@ import SearchBar from '../SearchBar/SearchBar';
 function App() {
   return (
     <div className="App">
-    <h1>ravenous</h1>
-    <SearchBar />
-    <BusinessList />
-  </div>
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
   )
 };
 
